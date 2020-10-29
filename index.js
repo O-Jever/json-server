967f6422fc8f2ff99d06f4171f1eae0b962112d2
Jever Osmanova <amidala274@gmail.com>
2020-10-29 20:24:02 +0300

    Cors added

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const jsonServer = require('json-server');
+const cors = require('cors');
 const server = jsonServer.create();
 const router = jsonServer.router('data.json');
 
+server.use(cors());
 server.use(router);
 server.listen(process.env.PORT || 3000, () => {
   console.log('JSON Server is running')
