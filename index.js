889134055dab2854298903998e0fa0d1d994a166
Jever Osmanova <amidala274@gmail.com>
2020-10-29 20:17:23 +0300

    Bug fix

---
+++ index.js
@@ -0,0 +1,8 @@
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('data.json');
+
+server.use(router);
+server.listen(process.env.PORT || 3000, () => {
+  console.log('JSON Server is running')
+});
